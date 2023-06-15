a5308280433eaf0fddb74407a58ca61ea4c14063
Wali Zaidi <i202429@nu.edu.pk>
2023-06-15 13:06:41 +0500

    Added in the frontend properly now, and cleaned up the template

--- backend/Tests/doItemRoute.test.js
+++ backend/Tests/doItemRoute.test.js
@@ -56,6 +56,22 @@ describe("Testing the to do list item routes", () => {
         expect(response.body.message).toBe({"message": "Error retrieving items"});
     });
 
+    it('Testing getting a specific list item for a user', async() => {
+        const response = await request(app).get('/list/1').send({
+            username: "testuser1"
+        });
+        
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe("Item retrieved successfully");
+    });
 
+    it('Testing the failure scenario for getting a specific list item for a user', async() => {
+        const response = await request(app).get('/list/1').send({
+            username: "testuse"
+        })
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body.message).toBe({"message": "Error retrieving item"});
+    });
 });
 
