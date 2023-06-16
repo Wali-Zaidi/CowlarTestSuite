c8582a0f728767aa5d38dbc34ce838d1f6fdb0b6
Wali Zaidi <i202429@nu.edu.pk>
2023-06-16 17:53:00 +0500

    trying to get the tests to work

--- frontend/src/Tests/LandingPage.test.js
+++ frontend/src/Tests/LandingPage.test.js
@@ -1,5 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import LandingPage from '../Pages/LandingPage';
+jest.mock('axios');
+
 
 test('renders mainDiv', () => {
     render(<LandingPage />);
